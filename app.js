193addb9e678828c100369d67f496d07c1c7548e
abhishek <arai57478@gmail.com>
2021-07-23 10:42:15 +0530

    complete

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+
 mongoose.connect("mongodb://localhost:27017/ApiDB", {
   useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify:false
 });
@@ -26,6 +27,22 @@ const contentSchema=new mongoose.Schema({
     expireToken:Date
 });
 const User=mongoose.model("User",contentSchema);
+const PostSchema=new mongoose.Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    postId:Number,
+    title:String,
+    des:String,
+    photo:String,
+    likes:[String],
+    comments:
+        [{
+        text:String,
+       commentedBy:String,
+    }
+    ] ,
+    postedBy:String
+},{timestamps:true});
+const Post=mongoose.model("Post",PostSchema);
 
 
 const transporter = nodemailer.createTransport(sendgridTransport({
@@ -129,7 +146,186 @@ app.route("/forgotpassword")
 
         })
     })
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.route("/deleteUser")
+.delete(function(req,res){
+    const userName=req.body.username;
+    const userPassword=req.body.password;
+    User.findOneAndDelete({username:userName},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+               
+                bcrypt.compare(userPassword, foundUser.password, function(err, result) {
+                    if(result===true){
+                        res.send("your have deleted your account succefully");
+                    }else{
+                        res.send("please try again wrong password ");
+                    }
+                });
+            }else{
+                res.send("User Not found ");
+            }
+        
+    }
+});
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////POST/////////////////////////////////////////////////////////////////////////////////////
+
+
+app.route("/post")
+.post( function (req, res) {
+    
+    const newPost= new Post({
+        _id:new mongoose.Types.ObjectId,
+        title:req.body.title,
+        des:req.body.des,
+        photo:req.body.photo,
+        postId:req.body.postId,
+        postedBy:req.body.postedBy
+    })
+    User.findOne({username:req.body.postedBy},function(err,foundUser){
+        if(foundUser){
+            newPost.save(function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.send("successfully saved the post !");
+                }
+            });
+        }else{ 
+           res.send("Please sign up first to save the post ")
+        }
+    });
+   
+   
+});
+app.route("/post/:postId")
+.get(function(req,res){
+    const postId =req.params.postId;
+    Post.findOne({postId:postId},function(err,foundPost){
+        if(foundPost){
+            res.send(foundPost);
+        }else{
+            res.send("no posts are matching");
+        }
+    });
 })
+.put(function(req,res){
+    Post.update({postId:req.params.postId},{title:req.body.title, des:req.body.des,photo:req.body.photo},{overwrite:true},function(err){
+        if(!err){
+            res.send("successfully updated post");
+        }else{
+            console.log(err);
+            res.send("no post find with such id");
+        }
+    });
+
+})
+.delete(function(req,res){
+    const userPassword=req.body.password;
+    Post.findOne({postId:req.params.postId},function(err,PostFound){
+        if(PostFound){
+            User.findOne({username:PostFound.postedBy},function(err,foundUser){
+                if(foundUser){
+               
+                    bcrypt.compare(userPassword, foundUser.password, function(err, result) {
+                        if(result===true){
+                                             Post.findOneAndDelete({postId:req.params.postId},function(err){
+                                                   if(!err){
+                                                     res.send("successfully deleted");
+                                                      }
+                                                   else{
+                                                      res.send(err);
+                                                       }
+                                                 });;
+                        }
+                        else{
+                            res.send("please try again wrong password ");
+                         }
+                     });
+                }
+              else{
+                  res.send("user not found");
+                }
+            });
+        }
+        else
+        { 
+         res.send("Post not Found ")
+         }
+     });
+         
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.route("/:postId/like")
+.put( function (req, res){
+    const postId =req.params.postId;
+    Post.findOne({postId:postId},function(err,PostFound){
+        if(PostFound){
+            if (!PostFound.likes.includes(PostFound.postedBy)){
+            Post.findOneAndUpdate({postId:postId},{ $push: { likes : PostFound.postedBy}},function(err){
+        if(!err)
+        {
+            res.send("liked");
+        }
+        else{
+            res.send(err);
+        }
+    })
+    } else{
+        Post.findOneAndUpdate({postId:postId},{ $pull: { likes : postedBy}},function(err){
+            if(!err)
+            {
+                res.send("disliked");
+            }
+            else{
+                res.send(err);
+            }
+        })
+    }
+}else{
+    res.send("post not found");
+}
+   
+    })
+});
+  
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.route("/:postId/comment")
+.put(function(req,res){
+  
+     Post.findOne({postId:req.params.postId},function(err,PostFound){
+        if(PostFound){
+                    const comment = 
+                        {
+                        text:req.body.text,
+                        postedBy:PostFound.postedBy
+                        };
+                    Post.findOneAndUpdate({postId:req.params.postId},{ $push: { comments : comment}},function(err){
+                        if(!err)
+                        {
+                            res.send("commented");
+                        }
+                        else{
+                            res.send(err);
+                        }
+                });
+        }else {
+            res.send("sorry ! no post is been found with this id ")
+        }
+    });
+});
+
+
 
 
 
